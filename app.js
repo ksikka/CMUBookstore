02c696b60e4f8f122d36555dee26249fc5b552a5
Karan Sikka <kssworld93@gmail.com>
2012-07-14 15:43:57 -0700

    making stuff more restful

--- app.js
+++ app.js
@@ -48,19 +48,19 @@ app.get('/', routes.index);
 app.all('/log_out',auth.logout);
 app.post('/andrew',auth.login);
 
-app.put('/user/books/selling/add',function(req,res){
+//app.put('/user/books/buying/');
+//app.del('/user/books/buying/');
+
+app.put('/user/books/selling/',function(req,res){
   siteUser = req.session.user;
-  console.log(siteUser['buying_ids']);
   book_id = parseInt(req.body.book_id);
-  console.log(book_id);
   book.Book.findOne({"_id":book_id}).run(function(err,doc) {
     if(!doc) {
-      res.send("Not a valid book");
+      res.send("Not a valid book",500);
     } else if(siteUser['selling_ids'].indexOf(book_id)!=-1) {
-      res.send("You already have this book on your sell list.");
+      res.send("You already have this book on your sell list.",409);
     } else {
       siteUser['selling_ids'].push(book_id);
-      console.log(siteUser._id);
       user.User.update({andrew_id:siteUser.andrew_id},{ selling_ids:siteUser['selling_ids'] },function(err){
         if(err) { console.log(err); }
         else { res.send("Added to purchase list."); }
@@ -68,6 +68,7 @@ app.put('/user/books/selling/add',function(req,res){
     }
   });
 });
+//app.del('/user/books/selling')
 app.get('/search',auth.requiresAuth,search.search);
 
 app.listen(3000, function(){
