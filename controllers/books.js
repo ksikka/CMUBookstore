32d93b4027b46eb902f50ebc1e152a723512bdad
Karan Sikka <kssworld93@gmail.com>
2012-07-15 18:40:39 -0700

    modularity

---
+++ controllers/books.js
@@ -0,0 +1,62 @@
+//import all the typical shit//
+var user = require('../models/user'),
+    book = require('../models/book');
+
+var removeFromBookList = function(req,res,action){
+  var listName = action+"ing_ids";
+  siteUser = req.session.user;
+  book_id = parseInt(req.body.book_id);
+  book.Book.findOne({"_id":book_id}).run(function(err,doc) {
+    if(!doc) {
+      res.send("Not a valid book",500);
+    } else if(siteUser[listName].indexOf(book_id)==-1) {
+      res.send("You can't remove this book if it's not on your "+action+" list.",409);
+    } else {
+      var indexOfBook = siteUser[listName].indexOf(book_id);
+      siteUser[listName] = siteUser[listName].slice(0,indexOfBook).concat(siteUser[listName].slice(indexOfBook+1,siteUser[listName].length));
+      updateDict = {};
+      updateDict[listName] = siteUser[listName];
+      user.User.update({"andrew_id":siteUser.andrew_id}, updateDict,function(err){
+        if(err) { console.log(err); }
+        else { res.send("Removed from "+action+" list."); }
+      });
+    }
+  });
+}
+
+var addToBookList = function(req,res,action){
+  var listName = action+"ing_ids";
+  siteUser = req.session.user;
+  book_id = parseInt(req.body.book_id);
+  book.Book.findOne({"_id":book_id}).run(function(err,doc) {
+    if(!doc) {
+      res.send("Not a valid book",500);
+    } else if(siteUser[listName].indexOf(book_id)!=-1) {
+      res.send("You already have this book on your "+action+" list.",409);
+    } else {
+      siteUser[listName].push(book_id);
+      updateDict = {};
+      updateDict[listName] = siteUser[listName];
+      user.User.update({"andrew_id":siteUser.andrew_id}, updateDict,function(err){
+        if(err) { console.log(err); }
+        else { res.send("Added to "+action+" list."); }
+      });
+    }
+  });
+}
+
+exports.addBookToBuyList = function(req,res) {
+  addToBookList(req,res,"buy");
+}
+
+exports.addBookToSellList = function(req,res) {
+  addToBookList(req,res,"sell");
+}
+
+exports.removeBookFromBuyList = function(req,res) {
+  removeFromBookList(req,res,"buy");
+}
+
+exports.removeBookFromSellList = function(req,res) {
+  removeFromBookList(req,res,"sell");
+}
