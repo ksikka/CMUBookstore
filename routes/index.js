3378a7062b1b72e2ae2b92da40b143209128755d
Karan Sikka <kssworld93@gmail.com>
2012-07-24 01:38:39 -0700

    dimensions are in place

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@
  */
 
 exports.index = function(req, res){
-  //res.render('index', { title: 'Express' });
-  res.render('splash', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 };
