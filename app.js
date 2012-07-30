724991b8c132157796d4e844f38ad1315205dca6
Karan Sikka <kssworld93@gmail.com>
2012-07-29 18:58:27 -0700

    separated concerns

--- app.js
+++ app.js
@@ -35,21 +35,19 @@ app.configure('production', function(){
 });
 
 // Importing controllers
-var search = require('./controllers/search');
-var auth = require('./controllers/auth');
-var books = require('./controllers/books');
+var search = require('./controllers/search')
+  , auth = require('./controllers/auth')
+  , books = require('./controllers/books')
+  , users = require('./controllers/users');
 //only for dev purposes
 var schedplus = require('./scheduleplus.js');
-var user = require('./models/user.js');
-var bcrypt = require('bcrypt');
+
 // Routes
 app.post('/extract',function(req,res){
     var surl = req.body.surl;
     console.log(surl);
     schedplus.scrape(surl,res);
 });
-
-//app.get('/', routes.index);
 
 app.get('/',function(req,res){
   auth.ifAuthElse(req,res
@@ -59,68 +57,11 @@ app.get('/',function(req,res){
 });
 
 app.post('/andrew',auth.login);
-app.all('/confirm/:andrew_id/:account_id',function(req,res){
-  var accountId = req.params.account_id;
-  var andrewId = req.params.andrew_id;
-  // see if it's in the db
-  user.User.findOne({_id:accountId,andrew_id:andrewId}).run(function(err,doc){
-    if(err) {
-      console.log(err);
-      res.send("404: not a real page",404);
-    }
-    else {
-      if(doc) {
-        //success
-        if(!doc.created_at) {
-          // give the user a chance to fill out all their settings
-          res.render('user_settings',{user:doc, title: "Create a new account"});
-        } else {
-          res.send("Account already created for this user. proceed to <a href='/'>the site</a>");
-        }
-      } else {
-        //wrong conf code, log this event.
-        console.log("failed attempt to guess conf code from email.");
-        console.log(andrewId+", "+accountId);
-        res.send("Cannot find that page",404);
-      }
-    }
-  });
-});
-app.post('/user',function(req,res){
-  id = req.body.emailconf;
-  password = req.body.password;
-  // do validations on the password
-  // store the password in the database
-  user.User.findOne({_id:id},function(err,user){
-    if(err) {
-      console.log(err);
-      res.send("",404);
-    } else {
-      if(user && !user.created_at) {
-        bcrypt.genSalt(10, function(err, salt) {
-          bcrypt.hash(password, salt, function(err, hash) {
-            // Store hash
-            user.password = hash;
-            user.created_at = new Date();
-            user.selling_ids = [];
-            user.buying_ids = []
-            user.last_login = null;
-            user.save(function(){
-              req.body.andrew_id = user.andrew_id;
-              req.body.password = password;
-              auth.login(req,res);
-            });
-          });
-        });
-      } else {
-        res.send("Sorry.",404);
-      }
-    }
-  });
-  // call the login function and redirect to the home page
-});
 app.all('/logincheck',auth.logincheck);
 app.all('/logout',auth.requiresAuth, auth.logout);
+
+app.all('/confirm/:andrew_id/:account_id',users.create_page);
+app.post('/user',users.set_password);
 
 app.get('/search',auth.requiresAuth,search.search);
 
---
+++ controllers/users.js
@@ -0,0 +1,65 @@
+// Importing controllers
+var auth = require('../controllers/auth');
+var user = require('../models/user.js');
+var bcrypt = require('bcrypt');
+
+exports.create_page = function(req,res){
+  var accountId = req.params.account_id;
+  var andrewId = req.params.andrew_id;
+  // see if it's in the db
+  user.User.findOne({_id:accountId,andrew_id:andrewId}).run(function(err,doc){
+    if(err) {
+      console.log(err);
+      res.send("404: not a real page",404);
+    }
+    else {
+      if(doc) {
+        //success
+        if(!doc.created_at) {
+          // give the user a chance to fill out all their settings
+          res.render('user_settings',{user:doc, title: "Create a new account"});
+        } else {
+          res.send("Account already created for this user. proceed to <a href='/'>the site</a>");
+        }
+      } else {
+        //wrong conf code, log this event.
+        console.log("failed attempt to guess conf code from email.");
+        console.log(andrewId+", "+accountId);
+        res.send("Cannot find that page",404);
+      }
+    }
+  });
+}
+
+exports.set_password = function(req,res){
+  id = req.body.emailconf;
+  password = req.body.password;
+  // do validations on the password
+  // store the password in the database
+  user.User.findOne({_id:id},function(err,user){
+    if(err) {
+      console.log(err);
+      res.send("",500);
+    } else {
+      if(user && !user.created_at) {
+        bcrypt.genSalt(10, function(err, salt) {
+          bcrypt.hash(password, salt, function(err, hash) {
+            // Store hash
+            user.password = hash;
+            user.created_at = new Date();
+            user.selling_ids = [];
+            user.buying_ids = []
+            user.last_login = null;
+            user.save(function(){
+              req.body.andrew_id = user.andrew_id;
+              req.body.password = password;
+              auth.login(req,res);
+            });
+          });
+        });
+      } else {
+        res.send("Sorry.",404);
+      }
+    }
+  });
+}
